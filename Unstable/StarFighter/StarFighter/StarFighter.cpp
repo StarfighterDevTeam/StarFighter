185144c44d7e172cd975c4f8e1875207bea9b469
Wister <bobwister@gmail.com>
2014-01-12 17:41:58 +0100

    Assets 2D : player, enemyXBOXlolomg, laser

--- Unstable/StarFighter/StarFighter/StarFighter.cpp
+++ Unstable/StarFighter/StarFighter/StarFighter.cpp
@@ -2,19 +2,21 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1600, 1200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
     {
-        sf::Event event;
+        //update
+		sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
+		//display
         window.clear();
         window.draw(shape);
         window.display();
