05d003d3af25029c0bb0c90119fb6d60584c0cda
Wister <bobwister@gmail.com>
2015-10-16 01:34:23 +0200

    Setting starting points for proto

--- Unstable/StarFighter/StarFighter/InGameState.cpp
+++ Unstable/StarFighter/StarFighter/InGameState.cpp
@@ -8,14 +8,14 @@ void InGameState::Initialize(Player player)
 	(*CurrentGame).init(this->mainWindow);
 	
 	//intégration placeholder
-	Ship* playerShip = new Ship(sf::Vector2f(SHIP_START_X, SHIP_START_Y), sf::Vector2f(0, 0), "Assets/2D/natalia.png", sf::Vector2f(64, 64), sf::Vector2f(32, 32), 3);
+	Ship* playerShip = new Ship(sf::Vector2f(100, 540), sf::Vector2f(0, 0), "Assets/2D/natalia.png", sf::Vector2f(64, 64), sf::Vector2f(32, 32), 3);
 	(*CurrentGame).SetPlayerShip(playerShip);
 	(*CurrentGame).addToScene((*CurrentGame).playerShip, LayerType::PlayerShipLayer, GameObjectType::PlayerShip);
 
 	GameObject* background = new GameObject(sf::Vector2f(960, 540), sf::Vector2f(0, 0), "Assets/2D/background.png", sf::Vector2f(1920, 1080), sf::Vector2f(960, 540));
 	(*CurrentGame).addToScene(background, LayerType::BackgroundLayer, GameObjectType::BackgroundObject);
 
-	Discoball* discoball = new Discoball(sf::Vector2f(1200, 540), sf::Vector2f(0, 0), "Assets/2D/discoball.png", sf::Vector2f(54, 54), sf::Vector2f(27, 27));
+	Discoball* discoball = new Discoball(sf::Vector2f(960, 540), sf::Vector2f(0, 0), "Assets/2D/discoball.png", sf::Vector2f(54, 54), sf::Vector2f(27, 27));
 	(*CurrentGame).addToScene(discoball, LayerType::DiscoballLayer, GameObjectType::DiscoballObject);
 
 	GameObject* goal_blue = new GameObject(sf::Vector2f(8, 540), sf::Vector2f(0, 0), "Assets/2D/goal_blue.png", sf::Vector2f(16, 200), sf::Vector2f(8, 100));
