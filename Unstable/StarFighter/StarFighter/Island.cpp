9db363974ce5cb94d448d1a73459e737cb5431d7
Wister <bobwister@gmail.com>
2019-05-02 10:14:09 +0200

    Displaying island texture

--- Unstable/StarFighter/StarFighter/Island.cpp
+++ Unstable/StarFighter/StarFighter/Island.cpp
@@ -13,6 +13,10 @@ Island::Island(int upcorner_x, int upcorner_y, int width, int height, int zone_c
 	m_seaport = NULL;
 	m_seaport = seaport;
 
+	//texture
+	Texture* texture;
+	texture = TextureLoader::getInstance()->loadTexture("2D/island_" + std::to_string(width) + "x" + std::to_string(height) + ".png", width * WATERTILE_SIZE, height * WATERTILE_SIZE);
+
 	//associating island tiles to island
 	for (int y = upcorner_y; y > upcorner_y - height; y--)//y : from top to bottom
 	{
@@ -21,7 +25,12 @@ Island::Island(int upcorner_x, int upcorner_y, int width, int height, int zone_c
 			WaterTile* tile = (*CurrentGame).m_waterzones[zone_coord_x][zone_coord_y]->m_watertiles[x][y];
 			tile->m_type = Water_Island;
 			tile->m_shape_container.setFillColor(sf::Color(201, 180, 95, 255));
-
+			
+			//the whole island corresponds to one texture, which is used as a spritesheet (1 tile = 1 frame). We're now associating the correct frame to each tile composing the island.
+			tile->setAnimation(texture, width, height);
+			tile->setAnimationLine(upcorner_y - y);
+			tile->setFrame(x - upcorner_x);
+			
 			m_tiles.push_back(tile);
 		}
 	}
