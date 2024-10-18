eef023cd1bc60146560bcabf819ea9d81d6de639
Wister <bobwister@gmail.com>
2024-10-18 10:48:05 +0200

    Init 2D assets

--- Unstable/StarFighter/StarFighter/Gameloop.cpp
+++ Unstable/StarFighter/StarFighter/Gameloop.cpp
@@ -5,12 +5,12 @@ extern Game* CurrentGame;
 Gameloop::Gameloop()
 {
 	//background
-	m_background = new GameObject(sf::Vector2f(960, 540), sf::Vector2f(0, 0), "2D/background.png", sf::Vector2u(1920, 1080));
+	m_background = new GameObject(sf::Vector2f(512, 512), sf::Vector2f(0, 0), "2D/water.jpg", sf::Vector2u(1024, 1024));
 	(*CurrentGame).addToScene(m_background, BackgroundLayer, BackgroundObject);
 	(*CurrentGame).m_map_size = m_background->m_size;
 
 	//ship
-	(*CurrentGame).m_playerShip = new Ship(sf::Vector2f(960, 540), sf::Vector2f(0, 0), "2D/natalia.png", sf::Vector2u(64, 64), sf::Vector2f(32, 32), 3, 1);
+	(*CurrentGame).m_playerShip = new Ship(sf::Vector2f(960, 540), sf::Vector2f(0, 0), "2D/ship_player_icon.png", sf::Vector2u(64, 128), sf::Vector2f(32, 32), 1, 2);
 	(*CurrentGame).addToScene((*CurrentGame).m_playerShip, PlayerShipLayer, PlayerShip);
 }
 
@@ -35,19 +35,5 @@ void Gameloop::Draw()
 
 void Gameloop::UpdateCamera(const float DTIME)
 {
-	(*CurrentGame).m_view.move(sf::Vector2f((*CurrentGame).m_playerShip->m_speed.x * DTIME, (*CurrentGame).m_playerShip->m_speed.y * DTIME));
-
-	//Map border constraints
-	const float x = (*CurrentGame).m_view.getSize().x / 2;
-	const float y = (*CurrentGame).m_view.getSize().y / 2;
-	const float a = (*CurrentGame).m_playerShip->getPosition().x;
-	const float b = (*CurrentGame).m_playerShip->getPosition().y;
-	if (a < x)
-		(*CurrentGame).m_view.setCenter(x, (*CurrentGame).m_view.getCenter().y);
-	if (a >(*CurrentGame).m_map_size.x - x)
-		(*CurrentGame).m_view.setCenter((*CurrentGame).m_map_size.x - x, (*CurrentGame).m_view.getCenter().y);
-	if (b < y)
-		(*CurrentGame).m_view.setCenter((*CurrentGame).m_view.getCenter().x, y);
-	if (b >(*CurrentGame).m_map_size.y - y)
-		(*CurrentGame).m_view.setCenter((*CurrentGame).m_view.getCenter().x, (*CurrentGame).m_map_size.y - y);
+	(*CurrentGame).m_view.setCenter((*CurrentGame).m_playerShip->getPosition());
 }
